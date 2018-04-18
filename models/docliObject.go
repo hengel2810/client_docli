7348e801df4c6f7ee3b44222c56cb8550c759b2b
Henrik Engelbrink <hengel2810@gmail.com>
2018-04-19 00:01:24 +0200

    Updated DocliObject -> Ports from container -> ([]int)

--- models/docliObject.go
+++ models/docliObject.go
@@ -5,12 +5,7 @@ type DocliObject struct {
 	UserId string `json:"user_id"`
 	OriginalName string `json:"image_name"`
 	UniqueId string `json:"unique_id"`
-	Ports []PortObject `json:"ports"`
+	Ports []int `json:"ports"`
 	Networks []string `json:"networks"`
 	Volumes []string `json:"volumes"`
-}
-
-type PortObject struct {
-	Container int `json:"container"`
-	Host int `json:"host"`
 }
